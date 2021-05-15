5399a515503b8bd2061ef6ce0ae5d31c1fe4d67d
Gregori Gomes Fernandes Vieira <gregori@datamotion.com.br>
2021-05-15 15:04:12 -0300

    first

---
+++ Atividade1.4/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade1._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double valorGasolina, quantidadeDeLitros, total;
+
+            valorGasolina = Convert.ToDouble(label3.Text);
+            quantidadeDeLitros = Convert.ToDouble(textBox1.Text);
+
+            total = valorGasolina * quantidadeDeLitros;
+
+            MessageBox.Show("R$ " + Convert.ToString(total), "Valor a pagar");
+        }
+    }
+}
